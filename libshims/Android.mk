4b7a0ac997f7c8142753e552c1a109fe0d72f645
Willi Ye <williye97@gmail.com>
2016-10-29 19:16:46 +0200

    g4: Shim old camera client to ims-camera
    Change-Id: I1b52f72beb953bd957952b5b7a660c269c2ef4b3

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -13,6 +13,10 @@
 # limitations under the License.
 
 LOCAL_PATH := $(call my-dir)
+CAMERA_CLIENT_LOCAL_PATH:= $(call my-dir)
+include $(call all-subdir-makefiles)
+
+# WVM SHIM
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := wvm.c
@@ -30,5 +34,49 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := rild_socket.c
 LOCAL_MODULE := rild_socket
 LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
+
+# CAMERA SHIM
+include $(CLEAR_VARS)
+
+LOCAL_PATH := $(CAMERA_CLIENT_LOCAL_PATH)
+
+LOCAL_SRC_FILES:= \
+	Camera.cpp \
+	CameraMetadata.cpp \
+	CaptureResult.cpp \
+	CameraParameters2.cpp \
+	ICamera.cpp \
+	ICameraClient.cpp \
+	ICameraService.cpp \
+	ICameraServiceListener.cpp \
+	ICameraServiceProxy.cpp \
+	ICameraRecordingProxy.cpp \
+	ICameraRecordingProxyListener.cpp \
+	camera2/ICameraDeviceUser.cpp \
+	camera2/ICameraDeviceCallbacks.cpp \
+	camera2/CaptureRequest.cpp \
+	camera2/OutputConfiguration.cpp \
+	CameraBase.cpp \
+	CameraUtils.cpp \
+	VendorTagDescriptor.cpp \
+	CameraParameters.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+	libcutils \
+	libutils \
+	liblog \
+	libbinder \
+	libhardware \
+	libui \
+	libgui \
+	libcamera_metadata
+
+LOCAL_C_INCLUDES += \
+	$(LOCAL_PATH)/include \
+	system/media/camera/include \
+	system/media/private/camera/include
+
+LOCAL_MODULE:= libshim_camera
 
 include $(BUILD_SHARED_LIBRARY)
