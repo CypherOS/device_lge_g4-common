a4e63d5715c2d1df46f538cf045ba60893bf6b45
Glen Kuhne <kuh@google.com>
2017-10-02 09:46:59 -0700

    g4: Add p2p_no_group_iface=1 to p2p_supplicant_overlay
    * Configures wpa_supplicant to use p2p0 mgmt iface instead of dynamically
      created p2p-p2p0-XYZ virtual interfaces for P2P group.
    
    Bug: 65524423
    Test: CtsVerifier WifiDirect
    Test: Manual verification
    Change-Id: I70582f114ab69684424aac41fbeb78fa7cc04847
    Merged-In: Id9add59aa122adb7f39c80252061a8f5d3cc55cd

--- product/bcm-wifi.mk
+++ product/bcm-wifi.mk
@@ -23,5 +23,6 @@ $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4339
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:/system/vendor/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:/system/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:/system/vendor/etc/wifi/p2p_supplicant_overlay.conf \
     frameworks/native/data/etc/android.hardware.wifi.xml:/system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:/system/etc/permissions/android.hardware.wifi.direct.xml
