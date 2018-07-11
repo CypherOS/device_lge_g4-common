1ec4479801bfaba7620b11df5d16f99894095617
pavlaras <theodoroupaulos@gmail.com>
2018-07-11 11:21:02 +0200

    g4: charger: Fix offline charger mode
    	Use healthd from our N
    
    Change-Id: Ic78628b7eeb92d905cdc7cdf574245750e2c57bc

--- product/power.mk
+++ product/power.mk
@@ -4,3 +4,10 @@ PRODUCT_PACKAGES += \
     android.hardware.health@1.0-service \
     android.hardware.power@1.0-impl \
     power.msm8992
+
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/ramdisk/sbin/healthd:root/sbin/healthd \
+        $(LOCAL_PATH)/ramdisk/res/images/font_log.png:root/res/images/font_log.png \
+        $(LOCAL_PATH)/ramdisk/res/images/charger/cm_battery_scale.png:root/res/images/charger/cm_battery_scale.png \
+        $(LOCAL_PATH)/ramdisk/res/images/charger/battery_scale.png:root/res/images/charger/battery_scale.png \
+        $(LOCAL_PATH)/ramdisk/res/images/charger/battery_fail.png:root/res/images/charger/battery_fail.png
