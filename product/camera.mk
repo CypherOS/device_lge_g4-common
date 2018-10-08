cbcc177b5c062fd90b0434dea90b6b5ab390b8c1
pavlaras <paul@cypheros.co>
2018-10-08 08:26:38 +0200

    g4: hals: camera: Build android.hardware.camera.provider@2.4-external-service
    Also build default camera hal for testing (will remove later)
    
    Change-Id: I652f218519b8e2d5b2ec59118a33cf347610224f

--- product/camera.mk
+++ product/camera.mk
@@ -2,6 +2,8 @@ PRODUCT_PACKAGES += \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-external-service \
+    camera.default \
     libqomx_core \
     libmm-qcamera
 
