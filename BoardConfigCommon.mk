47799ff89de3be030ae3c6c0d1d09ee858ac93d5
pavlaras <paul@cypheros.co>
2018-10-15 13:43:37 +0200

    g4: shims: Use libmmcamera_stillmore_lib.so from LG G5 h830 v30c (2/2)
    LG G5 has same camera sensor: imx234
    
    This shim fix error:
    Error opening frameproc library libmmcamera_stillmore_lib.so error dlopen failed: cannot locate symbol "_ZN4qrcp22StillMoreTunableParamsC1Ev" referenced by "/system/vendor/lib/libmmcamera_stillmore_lib.so"
    
    Change-Id: I0bc560b8b0a40f1d1cb4197954f5f033017b32f9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,7 +34,8 @@ TARGET_LD_SHIM_LIBS := \
     /system/vendor/lib64/libril-qc-qmi-1.so|/system/lib64/rild_socket.so \
     /system/lib64/libmdmcutback.so|/system/vendor/lib64/libqsap_shim.so \
     /system/vendor/lib64/libcneapiclient.so|system/vendor/lib64/libcne_shim.so \
-    /system/lib/libshim_camera.so:/system/lib/libcamera_client.so|libshim_cameraclient.so
+    /system/lib/libshim_camera.so:/system/lib/libcamera_client.so|libshim_cameraclient.so \
+    /system/vendor/lib/libmmcamera_stillmore_lib.so|/system/lib/libcamera_client.so
 
 # inherit from the proprietary version
 -include vendor/lge/g4-common/BoardConfigVendor.mk
