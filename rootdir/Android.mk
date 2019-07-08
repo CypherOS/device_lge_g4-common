26c241f98a582a67cf67663c3ef135b3d221cb13
kessaras <paul@cypheros.co>
2019-07-08 07:46:52 +0300

    g4-common: qcril: Get rid of deprecated init.class_main.sh
    We should reduce the scripts from init during boot
    
    Change-Id: I2242cb63d196ec515226971a4fc86e780b59b89a

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -102,14 +102,6 @@ LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE	    := init.class_main.sh
-LOCAL_MODULE_TAGS   := optional eng
-LOCAL_MODULE_CLASS  := ETC
-LOCAL_SRC_FILES	    := etc/init.class_main.sh
-LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE	    := init.msm8992.sensor.sh
 LOCAL_MODULE_TAGS   := optional eng
 LOCAL_MODULE_CLASS  := ETC
