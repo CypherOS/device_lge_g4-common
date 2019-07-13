8c6999d64fa32539455b13ee4e738510aee71359
kessaras <paul@cypheros.co>
2019-07-13 17:29:16 +0300

    g4-common: props: Remove ril props that are already in each variant device tree
    Change-Id: I6a3454a563a593a08db0a57d719cce947d680e0e

--- system_prop.mk
+++ system_prop.mk
@@ -125,7 +125,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
-    ril.subscription.types=NV,RUIM \
     persist.data.mode=concurrent \
     persist.data.netmgrd.qos.enable=true \
     persist.data.qmi.adb_logmask=0 \
@@ -135,7 +134,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.sib16_support=1 \
     ro.data.large_tcp_window_size=true \
     ro.use_data_netmgrd=true \
-    ro.telephony.default_network=12 \
     ro.ril.svlte1x=false \
     ro.ril.svdo=false \
     persist.radio.add_power_save=1 \
