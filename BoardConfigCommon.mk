f05be5b599fe6a2f529fcecbdb92c725a0e626b8
kessaras <paul@cypheros.co>
2019-08-10 09:34:33 +0300

    g4-common: We don't need a separated dt
    	We don't use it somewhere.
    
    Compress kernel image properly for future use as
    a prebuilt image to build boot.img if needed.
    
    Change-Id: I77ca26b63a46b39634e996edae6ace4beb9d8f47

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -149,8 +149,7 @@ BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 an
 BOARD_KERNEL_CMDLINE += user_debug=31 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 msm_rtb.filter=0x37 boot_cpus=0-5 lge_monitor_thermal.enable=1 fakebattery=enable
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_SEPARATED_DT := true
-BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/lge/msm8992
